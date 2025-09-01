3e999c5ff0fc9e2ef3e9d9d7391faaaa6afcfbe7
joss-coding <el.mundo.de.joshi1324@gmail.com>
2025-09-01 03:01:21 +0000

    modificación de include/System/memory_utils.h

--- include/System/memory_utils.h
+++ include/System/memory_utils.h
@@ -4,7 +4,7 @@
 
 void ini(Responses* res, int tabs);
 void fin(Responses* res, int tabs);
-void* (size_t size, char* name, Responses* res, int tabs);
+void* alloc(size_t size, char* name, Responses* res, int tabs);
 Response* alloc_buffer(Responses* res, int tabs, char* name);
 void free_buffer(Response* res);
 
